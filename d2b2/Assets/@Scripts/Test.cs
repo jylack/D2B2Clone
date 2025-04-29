7abb8014992cd57dbd36eb2f3d0bed7df992a40a
shsx477 <shsx477@gmail.com>
2025-04-29 17:08:01 +0900

    [feat] add firebase

---
+++ d2b2/Assets/@Scripts/Test.cs
@@ -0,0 +1,26 @@
+using Firebase;
+using Firebase.Auth;
+using Firebase.Extensions;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private FirebaseAuth auth;
+
+
+
+    private async void Start()
+    {
+        await FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(t => {
+            if (t.Result == DependencyStatus.Available)
+            {
+                auth = FirebaseAuth.DefaultInstance;
+                print(auth.App.Name);
+            }
+            else
+            {
+                Debug.LogError("Could not resolve all Firebase dependencies: " + t.Result);
+            }
+        });
+    }
+}
