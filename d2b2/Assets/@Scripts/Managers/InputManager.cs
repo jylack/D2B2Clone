85bf58543dd1c379b6e3f06acd16a7f3f545cabe
shsx477 <shsx477@gmail.com>
2025-04-30 14:15:11 +0900

    [feat] VR 관련 패키지 설치, InputManager 일부 작성

---
+++ d2b2/Assets/@Scripts/Managers/InputManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public delegate void OnHeadRotatingPerformHandler(Quaternion rotation);
+public delegate void OnHeadRotatingCancelHandler();
+
+public class InputManager : MonoBehaviour
+{
+    public event OnHeadRotatingPerformHandler OnHeadRotatingPerform;
+    public event OnHeadRotatingCancelHandler OnHeadRotatingCancel;
+
+    XRIDefaultInputActions inputActions;
+
+
+
+    private void Awake()
+    {
+        inputActions = new XRIDefaultInputActions();
+    }
+
+    private void Start()
+    {
+        inputActions.Enable();
+
+        inputActions.XRIHead.Rotation.performed += Rotation_performed;
+        inputActions.XRIHead.Rotation.canceled += Rotation_canceled;
+    }
+
+    
+
+    private void Rotation_performed(InputAction.CallbackContext obj)
+    {
+        var rotation = obj.ReadValue<Quaternion>();
+        OnHeadRotatingPerform?.Invoke(rotation);
+    }
+
+    private void Rotation_canceled(InputAction.CallbackContext obj)
+    {
+        OnHeadRotatingCancel?.Invoke();
+    }
+}
