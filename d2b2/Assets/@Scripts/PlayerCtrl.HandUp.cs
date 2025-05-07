3aa431deb8d551fe55c26875a0dcc9f5da6597d0
Fifow <lasldjf123@naver.com>
2025-05-07 13:25:01 +0900

    [feat] 주석처리

--- d2b2/Assets/@Scripts/PlayerCtrl.HandUp.cs
+++ d2b2/Assets/@Scripts/PlayerCtrl.HandUp.cs
@@ -12,15 +12,15 @@ public partial class PlayerCtrl : MonoBehaviour
 
     private void OnLeftHandUp(Vector3 handPosition)
     {
-        float currentHeadY = headTransform.position.y; //  매번 현재 머리 높이 가져오기
-        isLeftHandUp = handPosition.y > currentHeadY;
+        //float currentHeadY = headTransform.position.y; //  매번 현재 머리 높이 가져오기
+       // isLeftHandUp = handPosition.y > currentHeadY;
        // Debug.Log($"[왼손] 손 위치 Y: {handPosition.y:F2}, 머리 Y: {currentHeadY:F2}, 위? {isLeftHandUp}");
     }
 
     private void OnRightHandUp(Vector3 handPosition)
     {
-        float currentHeadY = headTransform.position.y; //  매번 현재 머리 높이 가져오기
-        isRightHandUp = handPosition.y > currentHeadY;
+        //float currentHeadY = headTransform.position.y; //  매번 현재 머리 높이 가져오기
+        //isRightHandUp = handPosition.y > currentHeadY;
         //Debug.Log($"[오른손] 손 위치 Y: {handPosition.y:F2}, 머리 Y: {currentHeadY:F2}, 위? {isRightHandUp}");
     }
     private void OnHeadRotation(Quaternion rotation)
