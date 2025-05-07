759f399c728804dd08450afaa71e146915893a13
jylack <jyl011202@gmail.com>
2025-05-07 12:44:28 +0900

    [feat] playerctrl 핸드무빙 클래스 추가

--- d2b2/Assets/@Scripts/PlayerCtrl.HandSwing.cs
+++ d2b2/Assets/@Scripts/PlayerCtrl.HandSwing.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public partial class PlayerCtrl : MonoBehaviour
 {
- 
 
+    bool isRightHandMoving = false;
+    bool isLeftHandMoving = false;
+
+
+
+    private void InitMoving()
+    {
+
+    }
 }
