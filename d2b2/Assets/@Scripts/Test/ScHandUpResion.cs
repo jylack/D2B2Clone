b546323b268eeab1a401772897e707357391ba0f
Fifow <lasldjf123@naver.com>
2025-05-12 18:42:15 +0900

    [Feat] 게이지 ui 추가

--- d2b2/Assets/@Scripts/Test/ScHandUpResion.cs
+++ d2b2/Assets/@Scripts/Test/ScHandUpResion.cs
@@ -25,7 +25,7 @@ public class ScHandUpResion : MonoBehaviour
     private void CheckHandUp(bool ledftHand, bool rightHand)
     {
         Debug.Log("ledftHand : " + ledftHand);
-        if (ledftHand == false)
+        if (ledftHand == false && rightHand == false)
         {
             handUpMissionClear = false;
         }
