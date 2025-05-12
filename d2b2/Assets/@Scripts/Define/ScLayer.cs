75af9f4b9309b5bb36bd031b0a088e4c115b4e08
shsx477 <shsx477@gmail.com>
2025-05-12 10:22:21 +0900

    [feat] ScLayer에 Player추가

--- d2b2/Assets/@Scripts/Define/ScLayer.cs
+++ d2b2/Assets/@Scripts/Define/ScLayer.cs
@@ -2,12 +2,15 @@
 {
     private const int MirrorIdx = 3;
     private const int NpcIdx = 6;
+    private const int PlayerIdx = 7;
 
-    public int MirrorIndex { get; private set; } = MirrorIdx;
-    public int NpcIndex { get; private set; } = NpcIdx;
+    public int MirrorIndex { get; private set; }    = MirrorIdx;
+    public int NpcIndex { get; private set; }       = NpcIdx;
+    public int PlayerIndex { get; private set; }    = PlayerIdx;
 
     public int MirrorMask { get; private set; } = Pow(MirrorIdx);
-    public int NpcMask { get; private set; } = Pow(NpcIdx);
+    public int NpcMask { get; private set; }    = Pow(NpcIdx);
+    public int PlayerMask { get; private set; } = Pow(PlayerIdx);
 
 
 
