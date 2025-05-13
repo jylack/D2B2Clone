79715a2a36e6704256c937bc462bfb98bcdf914f
Fifow <lasldjf123@naver.com>
2025-05-13 10:31:21 +0900

    [Feat] 레이어 추가

--- d2b2/Assets/@Scripts/Test/UIPlayerHsy.cs
+++ d2b2/Assets/@Scripts/Test/UIPlayerHsy.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+public enum UIType { LookAroundUI, HandUpUi}
 
 public class UIPlayerHsy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject lookAroundLeftProgress;
+    [SerializeField] private GameObject lookAroundRightProgress;
+    [SerializeField] private GameObject handUpProgressUI;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    
+
+
+
+    //private Dictionary<UIType, GameObject> uiDict;
+    //private void Awake()
+    //{
+    //    uiDict = new Dictionary<UIType, GameObject>();
+    //
+    //    GameObject look = Instantiate(lookAroundProgressUI);
+    //    look.SetActive(false);
+    //    uiDict.Add(UIType.LookAroundUI, look);
+    //
+    //
+    //    GameObject hand = Instantiate(handUpProgressUI);
+    //    hand.SetActive(false);
+    //    uiDict.Add(UIType.HandUpUi, hand);
+    //}
+    //public void ShowUI(UIType type)
+    //{
+    //    uiDict[type].SetActive(true);
+    //}
+    //public void HideUI(UIType type)
+    //{
+    //    uiDict[type].SetActive(false);
+    //}
+
+
 }
