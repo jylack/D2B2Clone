8c5caee36599ff628841fdafc02af6f40f6c4818
정현우 <slander.jung@gmail.com>
2025-05-29 18:02:21 +0900

    [feat] 화면설정 추가
    랜더링 스케일과 밝기 조정 옵션 구현

---
+++ d2b2/Assets/@Scripts/Scenes/Tutorial/InitTuto/ScDisplaySetting.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+public class ScDisplaySetting : MonoBehaviour
+{
+    [SerializeField] Slider renderScaleSlider;
+    [SerializeField] Slider brightnessSlider;
+    UniversalRenderPipelineAsset urp;
+    Light sceneLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        urp = GraphicsSettings.currentRenderPipeline as UniversalRenderPipelineAsset;
+        sceneLight = GameObject.Find("Directional Light").GetComponent<Light>();
+        ApplyRenderScaleSetting();
+        ApplyBrightnessSetting();
+    }
+
+    public void ApplyRenderScaleSetting()
+    {
+        if (!urp) return;
+        urp.renderScale = renderScaleSlider.value;
+    }
+
+    public void ApplyBrightnessSetting()
+    {
+        if (!urp) return;
+        sceneLight.intensity = brightnessSlider.value + 1;
+    }
+}
