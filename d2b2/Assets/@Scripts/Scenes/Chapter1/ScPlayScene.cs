7ded9514a5e08a54fb18b4e452c5d3b44d603c56
jylack <jyl011202@gmail.com>
2025-06-09 15:07:00 +0900

    [fix] sceneMgr 떄문에 됬다 안됬다 하던거 수정

--- d2b2/Assets/@Scripts/Scenes/Chapter1/ScPlayScene.cs
+++ d2b2/Assets/@Scripts/Scenes/Chapter1/ScPlayScene.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,22 +7,42 @@ public class ScPlayScene : ScSceneBase
 {
     [SerializeField] private List<ScDefine.ScScene> MovingSceneList;
 
-    private void Start()
+    private bool isMovableScene = false;
+    private string currentSceneName;
+
+    private async UniTaskVoid Init()
     {
-        bool isMovableScene = false;
-        string currentSceneName = SceneManager.GetActiveScene().name;
+        await UniTask.WaitUntil(() => Manager.Instance.SceneMgr != null);
 
         foreach (var sceneName in MovingSceneList)
         {
-            if (currentSceneName == sceneName.ToString())
+            //Debug.Log($"Checking Scene: {sceneName}");                        
+
+            if (currentSceneName == Manager.Instance.SceneMgr.GetSceneName(sceneName))
             {
+                //Debug.Log($"Scene {sceneName} 이동가능씬");
                 isMovableScene = true;
                 break;
             }
         }
 
         if (!isMovableScene)
+        {
+            //Debug.LogWarning($"Scene {currentSceneName} 이동불가씬");
             Manager.Instance.GameMgr.SetCurrentPlayerMoveSpeed(0);
+        }
+
     }
 
+    private void Start()
+    {
+        currentSceneName = SceneManager.GetActiveScene().name;
+        //Debug.Log($"Current Scene: {currentSceneName}");
+
+
+        Init().Forget();
+    }
+
+
 }
+
