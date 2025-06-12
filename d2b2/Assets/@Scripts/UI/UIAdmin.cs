fdf48bf996c99c37719adcc6faa28fda2b58f6bf
shsx477 <shsx477@gmail.com>
2025-06-12 10:16:00 +0900

    [fix] UI 수정 (관리자씬, 로그인씬)

--- d2b2/Assets/@Scripts/UI/UIAdmin.cs
+++ d2b2/Assets/@Scripts/UI/UIAdmin.cs
@@ -12,10 +12,6 @@ public class UIAdmin : UIBase
     [SerializeField] private TMP_InputField inputId;
     [SerializeField] private TMP_InputField inputPassword;
 
-    public bool IsLoaded => ScAdminScene.Instance?.IsLoaded ?? false;
-
-    private Transform hoveredChapterCard;
-
 
 
     private void Awake()
@@ -23,46 +19,17 @@ public class UIAdmin : UIBase
         Instance = this;
     }
 
-    private void Start()
-    {
-        Manager.Instance.InputMgr.OnTriggerPerform += InputMgr_OnTriggerPerform;
-    }
-
-    private void OnDestroy()
-    {
-        Manager.Instance.InputMgr.OnTriggerPerform -= InputMgr_OnTriggerPerform;
-    }
-
 
 
     public void OnCardHoverEntered(HoverEnterEventArgs args)
     {
-        if (!IsLoaded)
-            return;
-
-        hoveredChapterCard = args.interactableObject.transform;
-        args.interactableObject.transform.DOLocalMoveZ(-0.5f, 0.2f);
+        Transform target = args.interactableObject.transform;
+        target.DOLocalMoveZ(-0.5f, 0.2f).SetLink(target.gameObject);
     }
 
     public void OnCardHoverExited(HoverExitEventArgs args)
     {
-        if (!IsLoaded)
-            return;
-
-        args.interactableObject.transform.DOLocalMoveZ(0f, 0.2f);
-
-        if (hoveredChapterCard == args.interactableObject.transform)
-            hoveredChapterCard = null;
-    }
-
-
-
-    private void InputMgr_OnTriggerPerform()
-    {
-        if (hoveredChapterCard == null)
-            return;
-
-        ScAdminScene.Instance.SetIsLoadedFalse();
-        hoveredChapterCard.GetComponent<UIChapterCard>().Select();
+        Transform target = args.interactableObject.transform;
+        target.DOLocalMoveZ(0f, 0.2f).SetLink(target.gameObject);
     }
 }
